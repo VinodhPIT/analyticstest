a3aa8e2e4d293a92a322d0c4d5a86230b880ece8
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-01-21 11:14:26 +0530

    scsc

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+ 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DY31MLR2GR"></script>
+<script
+         dangerouslySetInnerHTML={{
+           __html:  ` window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+         
+           gtag('config', 'G-DY31MLR2GR');
+         `,}}
+
+></script>
+
       <body>
         <Main />
         <NextScript />
