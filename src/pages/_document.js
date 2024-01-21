9994f4290d228bee56091296452ec987ae4b40d2
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-01-21 12:44:57 +0530

    dcdc

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
- 
+      <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KWHQ36H2');
+              `,
+            }}
+          />
+
+
+
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-DY31MLR2GR"></script>
 <script
          dangerouslySetInnerHTML={{
@@ -18,6 +32,10 @@ export default function Document() {
 ></script>
 
       <body>
+     
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWHQ36H2"
+height="0" width="0"></iframe></noscript>
+
         <Main />
         <NextScript />
       </body>
--- src/pages/styleguide/index.js
+++ src/pages/styleguide/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import Comp from '@/components/TestComponent/Comp'
 export default function Styleguide() {
+
+  const handleClick = () => {
+    // Push a custom event to the data layer
+    window.dataLayer.push({
+      event: 'exploreMoreButtonClick',
+      buttonLabel: 'Explore More Tattoo',
+    });
+  
+    // Additional button click logic can go here
+  };
+
+  
+  
   return (
     <div>
       <div style={{ flexDirection: "row", justifyContent: "space-between" }}>
@@ -19,8 +32,7 @@ export default function Styleguide() {
         
         <p> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange</p>
 
-        <button>Explore more tattoo</button>
-
+        <button onClick={handleClick}>Explore more tattoo</button>;
 
         <Comp title={'Test'} desrip={'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don'} button={"Button stylegudie"}/>
 
