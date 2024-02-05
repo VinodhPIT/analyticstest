db97d9c9f8b0252339cccbf4fb25f5661a2acb4e
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-02-05 15:56:35 +0530

    wdwd

--- src/pages/analytics/login/index.jsx
+++ src/pages/analytics/login/index.jsx
@@ -24,7 +24,7 @@ const Login = () => {
 
   useEffect(() => {
     if (data?.user && status === "authenticated") {
-      router.push("/analytics/daschboard");
+      router.push("/analytics/dashboard");
     }
   }, [data, status]);
 
